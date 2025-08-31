a3e1b801ad20bbb5783fe2a12dca7d164cd51f17
zr <iamziarehman@gmail.com>
2025-09-01 00:14:58 +0100

    Remove all unncessary dependencies

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,18 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AppRouter from './App';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { Provider } from 'react-redux';
-import { store } from './store';
-
-const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
         <AppRouter />
-      </QueryClientProvider>
-    </Provider>
   </React.StrictMode>
 );
